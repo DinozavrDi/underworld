b876bca7ef8f1469e708038f24782daafae17492
DinozavrRus <AntonKoren21@yandex.ru>
2025-06-07 21:51:22 +0300

    NEXT

--- src/app/profile/actions.ts
+++ src/app/profile/actions.ts
@@ -3,6 +3,7 @@ import { Order } from "@/generated/prisma";
 export async function getOrderCabinet(userID: string): Promise<Order[]> {
   const result = await fetch(`/api/order/user/${userID}`, {
     method: "GET",
+    cache: "no-cache",
   });
 
   if (result.status !== 201) {
