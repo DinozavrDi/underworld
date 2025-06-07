cee2d5717fd3c97c3f3ef351fa5c944d4470b190
DinozavrRus <AntonKoren21@yandex.ru>
2025-06-07 20:58:13 +0300

    next

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,17 +9,18 @@ export default {
   theme: {
     extend: {
       animation: {
-        'fade-in': 'fadeIn 0.5s ease-out forwards',
-        'submarine': 'float 6s ease-in-out infinite', // 👈 добавляем сюда
+        "fade-in": "fadeIn 0.5s ease-out forwards",
+        submarine: "float 6s ease-in-out infinite", // 👈 добавляем сюда
       },
       keyframes: {
         fadeIn: {
-          '0%': { opacity: 0, transform: 'translateY(20px)' },
-          '100%': { opacity: 1, transform: 'translateY(0)' },
+          "0%": { opacity: "0", transform: "translateY(20px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
         },
-        float: { // 👈 и сюда
-          '0%, 100%': { transform: 'translateX(-50%) translateY(0)' },
-          '50%': { transform: 'translateX(-50%) translateY(-20px)' },
+        float: {
+          // 👈 и сюда
+          "0%, 100%": { transform: "translateX(-50%) translateY(0)" },
+          "50%": { transform: "translateX(-50%) translateY(-20px)" },
         },
       },
     },
